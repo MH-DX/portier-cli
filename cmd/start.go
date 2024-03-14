33714d75d2967d888f17ca98bc122bdf9f18095c
Mario Hammer <hammer.is.mario@googlemail.com>
2024-03-14 22:45:58 +0000

    feat(pre): add comment to start command

--- cmd/start.go
+++ cmd/start.go
@@ -8,6 +8,8 @@ import (
 	"github.com/spf13/cobra"
 )
 
+// TODO rename to "service" command, i.e. portier service start --config config.yaml --apiToken apiToken.yaml ...
+
 type startOptions struct {
 	Command      string
 	ConfigFile   string
