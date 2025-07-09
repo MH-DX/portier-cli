8d2ff0a2269272d1510fb4a0be071f13ed6411f8
Mario Hammer <hammer.is.mario@googlemail.com>
2025-07-09 13:57:19 +0000

    feat: better support for nsis

--- build-installer.bat
+++ build-installer.bat
@@ -2,17 +2,37 @@
 echo Building Portier CLI Windows Installer...
 
 :: Check if NSIS is available
+set "MAKENSIS_PATH="
 where makensis >nul 2>&1
-if %errorlevel% neq 0 (
-    echo ERROR: NSIS (makensis) not found in PATH
-    echo Please install NSIS from https://nsis.sourceforge.io/
-    exit /b 1
+if not errorlevel 1 (
+    set "MAKENSIS_PATH=makensis"
+    goto :nsis_found
 )
+
+:: Try common NSIS installation paths
+for %%p in (
+    "C:\Program Files\NSIS\makensis.exe"
+    "C:\Program Files (x86)\NSIS\makensis.exe"
+    "%LOCALAPPDATA%\Microsoft\WinGet\Packages\NSIS.NSIS_Microsoft.Winget.Source_8wekyb3d8bbwe\makensis.exe"
+) do (
+    if exist "%%p" (
+        set "MAKENSIS_PATH=%%p"
+        goto :nsis_found
+    )
+)
+
+echo ERROR: NSIS (makensis) not found
+echo Please install NSIS from https://nsis.sourceforge.io/
+echo Or add NSIS to your PATH environment variable
+exit /b 1
+
+:nsis_found
+echo Found NSIS: %MAKENSIS_PATH%
 
 :: Build the binaries first
 echo Building binaries...
 call build.bat
-if %errorlevel% neq 0 (
+if errorlevel 1 (
     echo Failed to build binaries
     exit /b 1
 )
@@ -41,8 +61,8 @@ if not exist "icon.ico" (
 
 :: Build the installer
 echo Building installer...
-makensis portier-installer.nsi
-if %errorlevel% neq 0 (
+"%MAKENSIS_PATH%" portier-installer.nsi
+if errorlevel 1 (
     echo Failed to build installer
     exit /b 1
 )
